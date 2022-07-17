7aa10e9fcc5844aea7ae11f923d5c4385836b2e9
Aipov Shamil <xphnx@ya.ru>
2022-07-17 16:54:53 +0300

    updated

--- src/hoc/UserAuthorized.tsx
+++ src/hoc/UserAuthorized.tsx
@@ -1,13 +1,11 @@
 import React, { FC } from 'react';
 import { Navigate } from 'react-router-dom';
-import { useAppSelector } from '../hooks/useAppSelector';
 
 interface UserAuthorizedProps {
   children: JSX.Element;
 }
 
 const UserAuthorized: FC<UserAuthorizedProps> = ({ children }) => {
-  //   const { user } = useAppSelector((state) => state.user);
   const isAuth = localStorage.getItem('isAuth');
 
   if (isAuth) return <Navigate to="/" replace />;
